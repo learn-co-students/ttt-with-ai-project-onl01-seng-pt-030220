d9b99cee31e7f3e569a0015ea45fa87f3a33f65c
Bartholomew Fountain <baertholomew@gmail.com>
2020-04-22 14:39:13 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,5 @@
 class Game
-
-  attr_accessor :board, :player_1, :player_2, :winner, :user_input
-
+  attr_accessor :board, :player_1, :player_2
   WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -16,23 +14,28 @@ class Game
     @player_1 = player_1
     @player_2 = player_2
     @board = board
-    @board.display
   end
 
-  def current_player
-     board.turn_count.odd? ? player_2 : player_1
+def current_player
+  if @board.turn_count.even? || @board.turn_count == 0
+    return @player_1
+  else
+    return @player_2
   end
+end
 
   def won?
-    WIN_COMBINATIONS.each do |combination|   #[0,1,2] which is board range (0-8)
-        if @board.cells[combination[0]] == @board.cells[combination[1]] &&
-          @board.cells[combination[1]] == @board.cells[combination[2]] &&
-          @board.taken?(combination[0]+1)
-          #Need to +1, because #taken? rspec test is working off user_input range (1-9)
-          return combination
-        end
+    on = false
+    WIN_COMBINATIONS.each do |comb|
+      if @board.cells[comb[0]] == "X" && @board.cells[comb[1]] == "X" &&  @board.cells[comb[2]] == "X"
+        on = comb
+        break
+      elsif @board.cells[comb[0]] == "O" && @board.cells[comb[1]] == "O" &&  @board.cells[comb[2]] == "O"
+          on = comb
+          break
       end
-    return false
+    end
+    on
   end
 
   def draw?
@@ -40,37 +43,33 @@ class Game
   end
 
   def over?
-     (won? || draw?) ? true : false
-     # IF board is not full, game is in progress (FALSE), ELSE, game is over (TRUE)
+    (won? || draw?) ? true : false
   end
 
   def winner
     if won?
-      combination = won?
-      @board.cells[combination[0]] # X or O
-    end
+     @board.cells[won?[0]] # X or O
+   end
   end
 
   def turn
-    puts "Please enter a number 1-9:"
-    @user_input = current_player.move(@board)
-    if @board.valid_move?(@user_input)
-      @board.update(@user_input, current_player)
-    else puts "Please enter a number 1-9:"
-      @board.display
+    input = current_player.move(@board)
+    if @board.valid_move?(input)
+      @board.update(input, current_player)
+    else
+      puts "Please enter a number 1-9"
       turn
     end
     @board.display
   end
 
-  def play
-    turn until over?
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cat's Game!"
+    def play
+      turn until over?
+      if won?
+        puts "Congratulations #{winner}!"
+      elsif draw?
+        puts "Cat's Game!"
+      end
     end
-  end
-
 
 end
