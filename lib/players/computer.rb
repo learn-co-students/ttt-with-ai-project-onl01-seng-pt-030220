cda3d4199df8a39002260d51f471530a1366a26e
YUKIKO IKEDA <applelele@gmail.com>
2020-05-10 18:06:16 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,19 +1,22 @@
 module Players
   class Computer < Player
 
-    # def move(board)
-    #   computer_move = (1..9).to_a.sample #==[1, 2, 3, 4, 5, 6, 7, 8, 9].sample
-    #   if board.valid_move?(computer_move)
-    #     board.update(computer_move, self)
-    #   else
-    #     move(board)
-    #   end
-    # end
-
     def move(board)
-      puts "Please enter your select number"
-      gets.strip
+      valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      computer_move = valid_moves.sample
+      # computer_move = (1..9).to_a.sample #==[1, 2, 3, 4, 5, 6, 7, 8, 9].sample
+      # if board.valid_move?(computer_move)
+      #   binding.pry
+      #   board.update(computer_move, self)
+      # else
+      #   move(board)
+      # end
     end
+
+    # def move(board)
+    #   puts "Please enter your select number"
+    #   gets.strip
+    # end
 
   end
 end
